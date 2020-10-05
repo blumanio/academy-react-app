063d1223d50aee8c78db94a64d52bb9cf206730d
melaammari <m.elaammari@reply.it>
2020-10-05 21:29:21 +0200

    added routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/layouts/Navbar";
+import Signup from './components/Routes/Signup';
+import Login from './components/Routes/Login';
+import {Route, BrowserRouter} from 'react-router-dom';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <BrowserRouter>
+      <div>
+      <Navbar/>
+      <Route path="/login" component={Login}/>
+      <Route path="/signup" component={Signup }/>
+
+
       </div>
+      
+
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/Routes/Signup.js
@@ -0,0 +1,18 @@
+
+
+import React, { Component } from 'react';
+
+class Signup extends Component {
+  render() {
+    return (
+      <p>
+          Signup page
+      </p>
+
+
+      
+    );
+  }
+}
+
+export default Signup;
